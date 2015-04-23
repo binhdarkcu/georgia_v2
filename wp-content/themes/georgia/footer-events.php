1052eb023f1e122ab11cfacf0bf57b5b7555fd12
Binh <binhdarkcu@gmail.com>
2015-04-23 03:41:18 -0700

    update page events

---
+++ wp-content/themes/georgia/footer-events.php
@@ -0,0 +1,49 @@
+<div id="footer-block">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6">
+                        <div class="footer-credit">
+                            <p>
+                                &copy; <a href="http://demo.toko.press/eventica-tecpro">Eventica</a> Powered by <a href="http://demo.toko.press/eventica-tecpro">Eventica</a> and <a href="https://wordpress.org/">Wordpress</a>.
+
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="footer-menu">
+                            <div id="secondary-menu" class="menu-footer-menu-container">
+                                <ul id="menu-footer-menu" class="footer-menu">
+                                    <li id="menu-item-1954" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1954"><a href="#">About</a></li>
+                                    <li id="menu-item-1955" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1955"><a href="#">Terms &#038; Condition</a></li>
+                                    <li id="menu-item-1956" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1956"><a href="#">FAQ</a></li>
+                                </ul>
+                            </div>		            		            		            <ul id="social-icon">
+                                <li>
+
+                                    <a href="#" title="Facebook" class="facebook"><i class="fa fa-facebook"></i></a>																															<a href="#" title="Twitter" class="twitter"><i class="fa fa-twitter"></i></a>																															<a href="#" title="gPlus" class="google-plus"><i class="fa fa-google-plus"></i></a>																															<a href="#" title="Linkedin" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div><!-- ./footer block -->
+    </div>
+    <div class="sb-slidebar sb-left sb-style-push"></div>
+	
+	<link rel='stylesheet' id='widget-calendar-pro-style-css' href='assets/css/widget-calendar-full.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe_events-widget-calendar-pro-style-css' href='assets/css/widget-calendar-theme.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe_events--widget-calendar-pro-override-style-css' href='assets/css/widget-calendar-theme.css?ver=3.9.1' type='text/css' media='all' />
+	
+    <script type='text/javascript' src='assets/js/add-to-cart.min.js?ver=2.3.7'></script>
+    <script type='text/javascript' src='assets/js/jquery.blockUI.min.js?ver=2.60'></script>
+
+    <script type='text/javascript' src='assets/js/cart-fragments.min.js?ver=2.3.7'></script>
+    <script type='text/javascript' src='http://demo.toko.press/eventica-tecpro/wp-content/themes/eventica-wp/js/superfish.js?ver=4.1.1'></script>
+    <script type='text/javascript' src='http://demo.toko.press/eventica-tecpro/wp-content/themes/eventica-wp/js/slidebars.js?ver=4.1.1'></script>
+    <script type='text/javascript' src='assets/js/owl.carousel.min.js?ver=2.0'></script>
+    <script type='text/javascript' src='assets/js/eventica.js?ver=1.3'></script>
+</body>
+</html>
---
+++ wp-content/themes/georgia/header-events.php
@@ -0,0 +1,64 @@
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="ie6 oldie" lang="en-US">
+<![endif]-->
+<!--[if IE 7]>
+<html class="ie7 oldie" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie8 oldie" lang="en-US">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+    <meta charset="UTF-8">
+    <title>Eventica | Event Calendar &amp; eCommerce</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="<?php echo get_bloginfo('template_url')?>/"></base>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+	
+	<link rel='stylesheet' id='tribe-events-bootstrap-datepicker-css-css' href='assets/css/datepicker.css?ver=4.1.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-custom-jquery-styles-css' href='assets/css/jquery-ui-1.8.23.custom.css?ver=4.1.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-full-calendar-style-css' href='assets/css/tribe-events-full.min.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-calendar-style-css' href='assets/css/tribe-events-theme.min.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-calendar-full-mobile-style-css' href='assets/css/tribe-events-full-mobile.min.css?ver=3.9.1' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='tribe-events-calendar-mobile-style-css' href='assets/css/tribe-events-theme-mobile.min.css?ver=3.9.1' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='woocommerce-layout-css' href='assets/css/woocommerce-layout.css?ver=2.3.7' type='text/css' media='all' />
+    <link rel='stylesheet' id='woocommerce-smallscreen-css' href='assets/css/woocommerce-smallscreen.css?ver=2.3.7' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='woocommerce-general-css' href='assets/css/woocommerce.css?ver=2.3.7' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-full-pro-calendar-style-css' href='assets/css/tribe-events-pro-full.min.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-calendar-pro-style-css' href='assets/css/tribe-events-pro-theme.min.css?ver=3.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='tribe-events-calendar-full-pro-mobile-style-css' href='assets/css/tribe-events-pro-full-mobile.min.css?ver=3.9.1' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='tribe-events-calendar-pro-mobile-style-css' href='assets/css/tribe-events-pro-theme-mobile.min.css?ver=3.9.1' type='text/css' media='only screen and (max-width: 768px)' />
+    <link rel='stylesheet' id='mailchimp-for-wp-checkbox-css' href='assets/css/checkbox.min.css?ver=2.2.8' type='text/css' media='all' />
+    <link rel='stylesheet' id='tokopress-fonts-css' href='http://fonts.googleapis.com/css?family=Noto+Sans%3A400%2C700%7CRaleway%3A400%2C700&#038;ver=1.3' type='text/css' media='all' />
+    <link rel='stylesheet' id='mailchimp-for-wp-form-css' href='assets/css/form.min.css?ver=2.2.8' type='text/css' media='all' />
+    <link rel='stylesheet' id='tokopress-woo-css' href='assets/css/style-woocommerce.css?ver=1.3' type='text/css' media='all' />
+    <link rel='stylesheet' id='style-theme-css' href='assets/css/style.css?ver=1.3' type='text/css' media='all' />
+    <style id='style-theme-inline-css' type='text/css'>
+        .woocommerce h1.page-title {
+            display: none;
+        }
+    </style>
+    <!--MY STYLE-->
+    <link rel='stylesheet' id='style-theme-css' href='css/all.css?ver=1.3' type='text/css' media='all' />
+
+    <script type='text/javascript' src='js/jquery.js?ver=1.11.1'></script>
+    <script type='text/javascript' src='js/jquery-migrate.min.js?ver=1.2.1'></script>
+
+    <style>
+        .page-title {
+            background-image: url(images/eventica-header-dummy-01.jpg);
+        }
+    </style>
+    <meta name="generator" content="Powered by Visual Composer - drag and drop page builder for WordPress." />
+    <!--[if IE 8]><link rel="stylesheet" type="text/css" href="assets/css/vc-ie8.css" media="screen"><![endif]-->
+    <style type="text/css">
+        .mc4wp-form input[name="_mc4wp_required_but_not_really"] {
+            display: none !important;
+        }
+    </style>
+</head>
