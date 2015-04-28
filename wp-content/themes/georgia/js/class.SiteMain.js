58d062bba90d3999885da104999681bb137277f0
binhlam <binhdarkcu@gmail.com>
2015-04-28 15:53:06 +0700

    update css

--- wp-content/themes/georgia/js/class.SiteMain.js
+++ wp-content/themes/georgia/js/class.SiteMain.js
@@ -1,9 +1,18 @@
 // JavaScript Document
 var SiteMain = (function() {
 	function init(){
-		
+		initEvent();
 	}
 	
+	function initEvent(){
+		jQuery('.sf-menu li.clslogin a').click(function(){
+			openPopup('#login-user');
+			return false;
+		});
+		jQuery('.popup .overlays').click(function(){
+			closePopup();
+		});
+	}
 	function createScrollbar(){
 		jQuery('.scrollbar').mCustomScrollbar({
 			theme:"light",
@@ -13,9 +22,19 @@ var SiteMain = (function() {
 		});
 	}
 	
+	function openPopup(idDiv){
+		jQuery('.popup').css('display','none');
+		jQuery(idDiv).css('display','block');
+	}
+	
+	function closePopup(){
+		jQuery('.popup').css('display','none');
+	}
 	
 	return {
 		init:init,
+		openPopup:openPopup,
+		closePopup:closePopup,
 		createScrollbar:createScrollbar
 	};
 	
