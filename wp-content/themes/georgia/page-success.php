d9789798e95f323622df3326a5be3bcdf07b5d9c
binhlam <binhdarkcu@gmail.com>
2015-04-30 13:51:46 +0700

    success page

---
+++ wp-content/themes/georgia/page-success.php
@@ -0,0 +1,38 @@
+<?php get_header();?>
+<body class="tribe-filter-live  tribe-events-uses-geolocation sticky-header-no wpb-js-composer js-comp-ver-4.4.2 vc_responsive events-list events-archive tribe-theme-eventica-wp tribe-events-page-template">
+    <div id="site-container" class="site-container sb-site-container">
+    	<?php get_template_part('tpl','menu');?>
+		 <section id="page-title" class="page-title events-title">
+            <div class="container">
+
+                <div class="breadcrumb-trail breadcrumb breadcrumbs">
+                    <span class="trail-begin"><a href="http://demo.toko.press/eventica-tecpro" title="Eventica">WORD LID</a></span>
+                    
+                </div>					
+                <h1>REGISTREER JE HIER</h1>
+            </div>
+        </section>
+
+
+        <div id="main-content" class="registerPage">
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-12">
+                    	<div class="registerBox">
+                    		<div class="intro">
+                    			<span>Bedankt voor uw registratie, we hebben uw aanvroaag goed ontvangen en contacteren u zo snel mogelijk terug</span>
+                    			
+                    		</div>
+                    		
+                    	</div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        </div>
+
+<?php get_footer();?>
