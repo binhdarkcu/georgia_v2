1b72b4ea087ac33e70d9da97ec641400cfd66034
binhlam <binhdarkcu@gmail.com>
2015-05-02 15:34:28 +0700

    update event page

--- wp-content/themes/georgia/tpl-events.php
+++ wp-content/themes/georgia/tpl-events.php
@@ -4,51 +4,57 @@
 
 		<?php
 			$added = array();
+			$datenum = array();
 			global $paged;
-			
 			
 	        $args_event = array(
 	            'post_type' 	 => 'post',
 	            'posts_per_page' => 5,
 			  	'paged'		=> $paged,		
-	            'order'			 => 'asc',
-	            'meta_key' => 'datetime'
+	            'order'			 => 'asc'
 	        );
 	        $queryEvents = new WP_Query($args_event);
-			
+			//print_r($queryEvents);
 			date_default_timezone_set( 'Europe/Amsterdam' );
 			setlocale(LC_ALL, 'nl_NL');
 			$months = explode( ',', ',januari,februari,maart,april,mei,juni,juli,augustus,september,october,november,december' );
 	        if ( $queryEvents->have_posts() ) : while ( $queryEvents->have_posts() ) : $queryEvents->the_post();
-	        	
 	        //foreach ($queryEvents as $event) {
-	            $url = wp_get_attachment_url(get_post_thumbnail_id($event->ID));
 				$datetime = get_field('datetime', $event->ID);
 				$date = DateTime::createFromFormat( 'dmY', $datetime , new DateTimeZone( 'Europe/Amsterdam' ));
 				$year = $date->format('Y');
 				$month = $months[ $date->format( 'n' ) ];
-				
+				array_push($added,$month." ".$year);
+				array_push($datenum,substr($datetime, 2, 2).$year);
 	    ?>
         <!-- Month / Year Headers -->
-        <span class='tribe-events-list-separator-month'><span><?php echo $month." ".$year;?></span></span>
+        
         <!-- Event  -->
-        <?php
-        	
-        	$argevent = array(
+        
+       <?php
+		endwhile; endif;//}//end for
+		
+		$dateAdded = arrayUnique($added, false);
+		$datenumAdded = arrayUnique($datenum);
+		$i = 0;
+		foreach ($datenumAdded as $numadded) {
+		?>
+			<span class='tribe-events-list-separator-month'><span><?php echo $dateAdded[$i];?></span></span>
+		<?php
+		$i++;
+        $argevent = array(
 			  'post_type'      => 'post',
 			  'posts_per_page' => -1,
 			  'meta_query'     => array(
 			    array(
 			      'key'     => 'datetime',
-			      'value'   => substr($datetime, 2, 2).$year,
+			      'value'   => $numadded,
 			      'compare' => 'RLIKE',
 			      'type'    => 'NUMBERRIC'
 			    ) 
 			  )
 			);
-			
 			$event_query = new WP_Query( $argevent );
-			
 			if ( $event_query->have_posts() ) : while ( $event_query->have_posts() ) : $event_query->the_post();
 				$datetime = get_field('datetime', get_the_ID());
 				$date = DateTime::createFromFormat( 'dmY', $datetime , new DateTimeZone( 'Europe/Amsterdam' ));
@@ -59,64 +65,61 @@
 				$month = $months[ $date->format( 'n' ) ];
 				$bigImg = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
 				//print_r(get_the_ID());	
-        ?>
-        <div id="post-2053" class="hentry vevent type-tribe_events post-2053 tribe-clearfix tribe-events-category-wordcamp tribe-events-venue-2054 tribe-events-first col-sm-6">
-
-
-            <div class="even-list-wrapper">
-                <div class="event-list-wrapper-top">
-                    <div class="tribe-events-event-image">
-                        <a href="<?php echo get_the_permalink(get_the_ID());?>" title="WordCamp Bratislava">
-                            <img width="400" height="200" src="<?php echo $bigImg;?>" class="attachment-blog-thumbnail wp-post-image" alt="Eventica Dummy Image 23" />
-                        </a>
-                    </div>
-
-                    <div class="tribe-events-event-date">
-                        <span class="dd"><?php echo $day;?></span>
-                        <span class="mm"><?php echo $month;?></span>
-                        <span class="yy"><?php echo $year;?></span>
-                    </div>
-                </div>
-
-                <div class="event-list-wrapper-bottom">
-                    <div class="wraper-bottom-left">
-                        <!-- Event Title -->
-                        <h2 class="tribe-events-list-event-title entry-title summary">
-                            <a class="url" href="<?php echo get_the_permalink(get_the_ID());?>" title="<?php echo get_the_title(get_the_ID());?>" rel="bookmark">
-                                <?php echo get_the_title(get_the_ID());?>
-                            </a>
-                        </h2>
-
-                        <!-- Event Meta -->
-                        <div class="tribe-events-event-meta vcard">
-                            <div class="author  location">
-
-                                <!-- Venue Display Info -->
-                                <div class="tribe-events-venue-details">
-                                    <a href="<?php echo get_the_permalink(get_the_ID());?>"><?php echo $loc['address'];?></a>
-                                </div> <!-- .tribe-events-venue-details -->
-
-                                <div class="time-details">
-                                    <?php echo $time;?>
-                                </div>
-
-                            </div>
-                        </div><!-- .tribe-events-event-meta -->
-                    </div>
-                    
-                </div>
-            </div>
-        </div><!-- .hentry .vevent -->
-        <?php
-			endwhile; endif;//}//end for
-	    ?>
-        <!-- Month / Year Headers -->
-        <!-- Event  -->
-        
-       <?php
-endwhile; endif;//}//end for
-	        
-	    ?>
+	        ?>
+	        <div id="post-2053" class="hentry vevent type-tribe_events post-2053 tribe-clearfix tribe-events-category-wordcamp tribe-events-venue-2054 tribe-events-first col-sm-6">
+	
+	
+	            <div class="even-list-wrapper">
+	                <div class="event-list-wrapper-top">
+	                    <div class="tribe-events-event-image">
+	                        <a href="<?php echo get_the_permalink(get_the_ID());?>" title="WordCamp Bratislava">
+	                            <img width="400" height="200" src="<?php echo $bigImg;?>" class="attachment-blog-thumbnail wp-post-image" alt="Eventica Dummy Image 23" />
+	                        </a>
+	                    </div>
+	
+	                    <div class="tribe-events-event-date">
+	                        <span class="dd"><?php echo $day;?></span>
+	                        <span class="mm"><?php echo $month;?></span>
+	                        <span class="yy"><?php echo $year;?></span>
+	                    </div>
+	                </div>
+	
+	                <div class="event-list-wrapper-bottom">
+	                    <div class="wraper-bottom-left">
+	                        <!-- Event Title -->
+	                        <h2 class="tribe-events-list-event-title entry-title summary">
+	                            <a class="url" href="<?php echo get_the_permalink(get_the_ID());?>" title="<?php echo get_the_title(get_the_ID());?>" rel="bookmark">
+	                                <?php echo get_the_title(get_the_ID());?>
+	                            </a>
+	                        </h2>
+	
+	                        <!-- Event Meta -->
+	                        <div class="tribe-events-event-meta vcard">
+	                            <div class="author  location">
+	
+	                                <!-- Venue Display Info -->
+	                                <div class="tribe-events-venue-details">
+	                                    <a href="<?php echo get_the_permalink(get_the_ID());?>"><?php echo $loc['address'];?></a>
+	                                </div> <!-- .tribe-events-venue-details -->
+	
+	                                <div class="time-details">
+	                                    <?php echo $time;?>
+	                                </div>
+	
+	                            </div>
+	                        </div><!-- .tribe-events-event-meta -->
+	                    </div>
+	                    
+	                </div>
+	            </div>
+	        </div><!-- .hentry .vevent -->
+	        <?php
+				endwhile; endif; //end for
+		    ?>
+	        <!-- Month / Year Headers -->
+	        <!-- Event  -->
+		<?php } ?>
+          
     </div>
 
 </div><!-- .tribe-events-loop -->
@@ -125,12 +128,12 @@ endwhile; endif;//}//end for
 
     <!-- Footer Navigation -->
 
-    <div class="tribe-events-pagination pagination clearfix" style="display: block!important;">
+    <div class="tribe-events-pagination pagination clearfix" style="display: none!important;">
         <h3 class="tribe-events-visuallyhidden">Events List Navigation</h3>
         <?php global $totalPage;
 		 	$totalPage = distinctPost($datetime);
 		 ?>
-        <?php echo bt_paginate($totalPage);?>
+        <?php //echo bt_paginate($totalPage);?>
         
     </div>
 </div>
