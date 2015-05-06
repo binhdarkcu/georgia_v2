fbeca2ce62c4bac58fb1d49b134979b590939e50
Thai Nguyen <thai@bliss-interactive.net>
2015-05-07 02:41:27 +0700

    a

--- wp-content/themes/georgia/header.php
+++ wp-content/themes/georgia/header.php
@@ -83,7 +83,7 @@
 
             $event_query = query_posts( $argevent );
 
-            if ( have_posts($event_query->$post) ) {
+            /*if ( have_posts($event_query->$post) ) {
                 while ( have_posts($event_query->$post) ) {
                     the_post($event_query->$post);
 
@@ -95,19 +95,29 @@
                     $month = substr($datetime, 2, 2);
 
                     $title = get_the_title(get_the_ID());
-                    $urlevent = get_the_permalink(get_the_ID());
 
                     $str1Array .= "'".$year.'-'.$month.'-'.$day."':{title:'".$title."',date:'".$year.'-'.$month.'-'.$day."',url:'".$urlevent."'},";
                     $str2Array .= "'".get_the_ID()."':{'".$year.'-'.$month.'-'.$day."':{title:'".$title."',url:'".$urlevent."'},},";
                 } // end while
                 wp_reset_postdata();
-            } // end if
+            } // end if*/
+            foreach ($event_query as $event) {
+                $i++;
+                $url = wp_get_attachment_url(get_post_thumbnail_id($event->ID));
+                $datetime = get_field('datetime', $event->ID);
+                $day = substr($datetime, 0, 2); // 13052015
+                $year = substr($datetime, -4);
+                $month = substr($datetime, 2, 2);
 
+                $title = get_the_title($event->ID);
+
+                $str1Array .= "'".$year.'-'.$month.'-'.$day."':{title:'".$title."',date:'".$year.'-'.$month.'-'.$day."',url:'".$urlevent."'},";
+                $str2Array .= "'".$event->ID."':{'".$year.'-'.$month.'-'.$day."':{title:'".$title."',url:'".$urlevent."'},},";
+			}
 
             ?>
 
         var calendar_events = {<?php echo $str1Array;?>};
-
         var pageurl = '<?php echo get_bloginfo('url')?>';
     </script>
 </head>
