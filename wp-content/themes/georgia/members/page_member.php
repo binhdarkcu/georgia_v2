e1c707454f6b0c2cb61a82194296aafecb093313
binhdarkcu <binhdarkcu@gmail.com>
2015-05-14 14:52:59 +0700

    edit member

--- wp-content/themes/georgia/members/page_member.php
+++ wp-content/themes/georgia/members/page_member.php
@@ -122,7 +122,19 @@ function process_bulk_action() {
 function process_edit_action() {
     
     //Detect when a bulk action is being triggered...
-    if( 'edit'===$this->current_action() ) {?>
+    if( 'edit'===$this->current_action() ) {
+		if(!empty($_REQUEST['p_email'])) {
+			$link = admin_url().'admin.php?page=view_member';
+			echo "<script>setTimeout(function(){window.location.href = '".$link."';},10);</script>";
+		}
+		else {
+			echo "<script>alert('a')</script>";
+		}
+		echo admin_url();
+		global $wpdb;
+		$query = 'SELECT * FROM wp_members WHERE id = '.$_GET['id'];
+		$member = $wpdb->get_row($query, ARRAY_A);
+	?>
     	<script src="<?php echo bloginfo('template_url')?>/js/jquery.js?ver=1.11.1"></script>
     	<script src="<?php echo bloginfo('template_url')?>/members/js/jquery-ui-1.10.1.min.js"></script>
     	<link href="<?php echo bloginfo('template_url')?>/members/css/jquery-ui-1.10.1.css" rel="stylesheet">
@@ -140,7 +152,7 @@ function process_edit_action() {
 		</script>
     	<div class="registerPage ">
     		<div class="registerBox">
-	    		<form action="" method="">
+	    		<form action="" method="post">
 	    			<h3>Edit member</h3>
 	    			<div class="informationBox">
 						<div class="reg-left">
@@ -148,41 +160,41 @@ function process_edit_action() {
 							<div class="reg-row">
 								<div class="col1">
 									<label>Naam<span class="red">*</span></label>
-									<input type="text" name="p_naam" value="" />
+									<input type="text" name="p_naam" value="<?php echo $member['p_naam']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Voornaam<span class="red">*</span></label>
-									<input type="text" name="p_voornaam" value="" />
+									<input type="text" name="p_voornaam" value="<?php echo $member['p_voornaam']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Geboortedatum<span class="red">*</span></label>
-									<input id="date_geboortedatum" type="text" name="p_geboortedatum" value="" />
+									<input id="date_geboortedatum" type="text" name="p_geboortedatum" value="<?php echo $member['p_geboortedatum']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Geboorteplaats<span class="red">*</span></label>
-									<input id="date_geboorteplaats" type="text" name="p_geboorteplaats" value=""  />
+									<input id="date_geboorteplaats" type="text" name="p_geboorteplaats" value="<?php echo $member['p_geboorteplaats']; ?>"  />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Straat<span class="red">*</span></label>
-									<input type="text" name="p_straat" value="" />
+									<input type="text" name="p_straat" value="<?php echo $member['p_straat']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Nr.<span class="red">*</span></label>
-									<input type="text" name="p_nr" value="" />
+									<input type="text" name="p_nr" value="<?php echo $member['p_nr']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Postcode<span class="red">*</span></label>
-									<input type="text" name="p_postcode" value="" />
+									<input type="text" name="p_postcode" value="<?php echo $member['p_postcode']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Plaats<span class="red">*</span></label>
-									<input type="text" name="p_plaats" value="" />
+									<input type="text" name="p_plaats" value="<?php echo $member['p_plaats']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
@@ -200,32 +212,31 @@ function process_edit_action() {
 	                                }
 	                                ?>
 									<select name="p_land">
-										<option value="0">Please select...</option>
-	                                    <?php echo $stroption_region_location;?>
+	                                    <?php echo str_replace('>'.$member['p_land'].'<', 'selected>'.$member['p_land'].'<', $stroption_region_location);?>
 									</select>
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Telefoon</label>
-									<input type="text" name="p_telefoon" value="" />
+									<input type="text" name="p_telefoon" value="<?php echo $member['p_telefoon']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Fax</label>
-									<input type="text" name="p_fax" value="" />
+									<input type="text" name="p_fax" value="<?php echo $member['p_fax']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>GSM<span class="red">*</span></label>
-									<input type="text" name="p_gsm" value="" />
+									<input type="text" name="p_gsm" value="<?php echo $member['p_gsm']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Privé emailadres<span class="red">*</span></label>
 									
-									<input type="text" name="p_email" value="" id="p_email"/>
+									<input type="text" name="p_email" value="<?php echo $member['p_email']; ?>" id="p_email"/>
 									<input name="ajaxurl" type="hidden" class="ajaxurl" value="<?php echo bloginfo('home').'/wp-admin/admin-ajax.php'; ?>"/>
 									<input type="hidden" value="<?php echo get_site_url();?>" class="siteurl"/>
 									<input name="action" type="hidden" class="action" value="check_user_email"/>
@@ -234,20 +245,20 @@ function process_edit_action() {
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Linkedin Profiel pagina</label>
-									<input type="text" name="p_likedin" value="" />
+									<input type="text" name="p_likedin" value="<?php echo $member['p_likedin']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Password<span class="red">*</span></label>
-									<input type="password" name="p_password" value="" />
+									<input type="password" name="p_password" value="<?php echo $member['p_password']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Profielfoto</label>
 									<div class="pictureUpload">
-										<img src="<?php echo bloginfo('template_url')?>/images/pic.png" class="imgPreview" style="width: 48px; height: 38px;"/>
+										<img src="<?php echo bloginfo('home')?>/wp-content/uploads/avatar/<?php echo $member['p_picture'];?>" class="imgPreview" style="width: 48px; height: 38px;"/>
 										<div class="fileUpload ">
 											<span>UPLOAD FOTO</span>
 											<input type="file" class="upload" name="p_picture"/>
@@ -261,11 +272,11 @@ function process_edit_action() {
 							<div class="reg-row">
 								<div class="col1">
 									<label>Naam van firma/organisatie<span class="red">*</span></label>
-									<input type="text" name="b_naam" value="" />
+									<input type="text" name="b_naam" value="<?php echo $member['b_naam']; ?>" />
 								</div>
 								<div class="col2">
 									<label>(Hoofd) Functie<span class="red">*</span></label>
-									<input type="text" name="b_hoofd" value="" />
+									<input type="text" name="b_hoofd" value="<?php echo $member['b_hoofd']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
@@ -283,9 +294,7 @@ function process_edit_action() {
 	                                }
 	                                ?>
 									<select name="b_firma">
-										<option value="0">Please select...</option>
-	
-										<?php echo $stroption_business_sector;?>
+										<?php echo str_replace('>'.$member['b_firma'].'<', 'selected>'.$member['b_firma'].'<', $stroption_business_sector);?>
 									</select>
 								</div>
 							</div>
@@ -293,21 +302,21 @@ function process_edit_action() {
 							<div class="reg-row">
 								<div class="col1">
 									<label>Straat<span class="red">*</span></label>
-									<input type="text" name="b_straat" value="" />
+									<input type="text" name="b_straat" value="<?php echo $member['b_straat']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Nr.<span class="red">*</span></label>
-									<input type="text" name="b_nr" value="" />
+									<input type="text" name="b_nr" value="<?php echo $member['b_nr']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Postcode<span class="red">*</span></label>
-									<input type="text" name="b_postcode" value="" />
+									<input type="text" name="b_postcode" value="<?php echo $member['b_postcode']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Plaats<span class="red">*</span></label>
-									<input type="text" name="b_plaats" value="" />
+									<input type="text" name="b_plaats" value="<?php echo $member['b_plaats']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
@@ -315,47 +324,48 @@ function process_edit_action() {
 									<label>Land<span class="red">*</span></label>
 	
 									<select name="b_land">
-	                                    <?php echo $stroption_region_location;?>
+	                                    <?php echo str_replace('>'.$member['b_land'].'<', 'selected>'.$member['b_land'].'<', $stroption_region_location);?>
 									</select>
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="col1">
 									<label>Telefoon</label>
-									<input type="text" name="b_telefoon" value="" />
+									<input type="text" name="b_telefoon" value="<?php echo $member['b_telefoon']; ?>" />
 								</div>
 								<div class="col2">
 									<label>Fax</label>
-									<input type="text" name="b_fax" value="" />
+									<input type="text" name="b_fax" value="<?php echo $member['b_fax']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>GSM</label>
-									<input type="text" name="b_gsm" value="" />
+									<input type="text" name="b_gsm" value="<?php echo $member['b_gsm']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Emailadres</label>
-									<input type="text" name="b_email" value="" />
+									<input type="text" name="b_email" value="<?php echo $member['b_email']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Website bedrijf/organisatie</label>
-									<input type="text" name="b_organisatie" value="" />
+									<input type="text" name="b_organisatie" value="<?php echo $member['b_organisatie']; ?>" />
 								</div>
 							</div>
 							<div class="reg-row">
 								<div class="colfull">
 									<label>Andere functies en mandaten</label>
-									<input type="text" name="b_functies" value="" />
+									<input type="text" name="b_functies" value="<?php echo $member['b_functies']; ?>" />
 								</div>
 							</div>	
 						</div>
 						<div class="clear"></div>
 					</div>
+					<input type="submit" value="Submit"/>
 	    		</form>
 	    	</div>
     	</div>
