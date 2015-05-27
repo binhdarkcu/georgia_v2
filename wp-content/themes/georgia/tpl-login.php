d80d96b11f1959d636b6bf3d3a64a13840c42a05
binhdarkcu <binhdarkcu@gmail.com>
2015-05-27 17:36:34 +0700

    update tpl-login

--- wp-content/themes/georgia/tpl-login.php
+++ wp-content/themes/georgia/tpl-login.php
@@ -1,47 +1,6 @@
 <div class="popup" id="login-user" style="display: none;">
-	<?php
-	    
-		$logged = false;
-	    	$message = "";
-		    if(isset($_POST['login_email']))
-		    {
-		        global $wpdb;
-		        $email = $_POST['login_email'];
-		        $password = sha1($_POST['p_password']);
-		        print_r($password);
-		        $results = $wpdb->get_row("SELECT * FROM wp_members WHERE p_email = '$email' and p_password = '$password'");
-		        if($results){
-		            $data = array();
-		            foreach ($results as $key => $value) {
-		                $data[$key] = $value;
-		            }
-		            $logged = true;
-		            $message = "Login success";
-		            unset($data['p_password']);
-		            $_SESSION['user'] = $data;
-		            $link = get_site_url().'/leden';
-		            echo "<script>setTimeout(function(){window.location.href = '$link';},0);</script>";
-		        }
-		        else{
-		            echo '<script language="javascript">';
-					//echo 'alert("Login fail.")';
-					echo '</script>';
-					$link = get_site_url().'/loginfail';
-					echo "<script>setTimeout(function(){window.location.href = '$link';},0);</script>";
-		        }
-		     }
-	
-	?>
 	<div class="overlays"></div>
 	<form id="loginForm" method="post" class="" action="">
-        <?php
-            if($message != "")
-            {
-                $alert = $logged == true ? "alert-success" : "alert-danger";
-                echo '<div class="alert '.$alert.'">'.$message.'</div>';
-            }
-        ?>
-		<div id="login-error" class="alert alert-danger"></div>
 		<input name="ajaxurl" type="hidden" class="ajaxurl" value="<?php echo bloginfo('home').'/wp-admin/admin-ajax.php'; ?>"/>
 		<div class="popup-content">
 			<img src="images/logo.png"/>
