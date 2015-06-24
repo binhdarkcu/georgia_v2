0819272b643c3e9f7d344ad41743688612f0fd67
Binh <binhdarkcu@gmail.com>
2015-06-24 00:14:56 -0700

    aa

--- wp-content/themes/georgia/index.php
+++ wp-content/themes/georgia/index.php
@@ -126,14 +126,15 @@
 	                                                	<input name="action" type="hidden" class="action" value="<?php echo empty($isjoin) ? 'add' : 'cancel'; ?>_event"/>
 	                                                   
 	                                                    <?php
+															$limit_time_to_cancel = is_numeric(get_field('limit_time_to_cancel', 'option')) ? get_field('limit_time_to_cancel', 'option'):2;
 	                                                    	if(!$isjoin){
-																if($diff > 2)	{
+																if($diff > $limit_time_to_cancel)	{
 	                                                    ?>
 															<a class="btn" rel="external" data-user-id="<?php echo $_SESSION['user']['id'];?>" data-event-id="<?php echo $near->ID;?>" id="addEvent" href="javascript:void(0);">
 																IK KOM
 															</a>
 	                                                    <?php } }else {
-	                                                    	if($diff > 2)	{
+	                                                    	if($diff > $limit_time_to_cancel)	{
 	                                                    ?>
 	                                                    <a class="btn" rel="external" data-user-id="<?php echo $_SESSION['user']['id'];?>" data-event-id="<?php echo $near->ID;?>" id="addEvent" href="javascript:void(0);">
 	                                                        CANCEL
