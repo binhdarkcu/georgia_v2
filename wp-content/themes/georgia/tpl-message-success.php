cb6fb56d6e40fd5995ddba1d335dfb03aba3f263
Binh <binhdarkcu@gmail.com>
2015-07-02 23:23:06 -0700

    aaaa

--- wp-content/themes/georgia/tpl-message-success.php
+++ wp-content/themes/georgia/tpl-message-success.php
@@ -3,7 +3,7 @@
 		<div class="popup-content">
 			<a href="javascript:void(0)" class="close" onclick="window.location.reload()">x</a>
 			<h3>U hebt zich nu ingeschreven voor dit evenement</h3>
-			<p>indien u niet wenst te komen gelieve dan binnen de 24 uur voor aanvang via de site te annuleren</p>
+			<p>indien u niet wenst te komen gelieve dan binnen de 48 uur voor aanvang via de site te annuleren</p>
 		</div>
 	</div>
 </div>
